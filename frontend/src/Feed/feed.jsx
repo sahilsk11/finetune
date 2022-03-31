427f565d56776931fbcd0f5d2dfb5bf7d4013edb
qhyltong <qhyltong@purdue.edu>
2022-03-31 15:24:48 -0400

    filter by genre and display saved posts

--- frontend/src/Feed/feed.jsx
+++ frontend/src/Feed/feed.jsx
@@ -72,6 +72,110 @@ export default function Feed() {
 
   }, [])
 
+  function displayMyFeed() {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        auth_token: localStorage.getItem("auth_token"),
+        username: localStorage.getItem("username"),
+      }
+    };
+    fetch(API_URL + "/view_feed", requestOptions)
+      .then(res => res.json())
+      .then(data => {
+        setLoading(false);
+        console.log("get  post request back is: ", data);
+        console.log(data);
+        if (data !== "failed") {
+          setPosts(data.sort((a,b)=>b.post_id-a.post_id));
+          setError(null);
+        } else {
+          //alert(data);
+          setError(data);
+        }
+      })
+      .catch(err => {
+        setLoading(false)
+        console.log("can not get  posts: " + err);
+        setError("Can not connect to server!");
+      });
+  }
+
+  function displaySavedPosts() {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        auth_token: localStorage.getItem("auth_token"),
+        username: localStorage.getItem("username"),
+        profile_user: localStorage.getItem("username")
+      }
+    };
+    fetch(API_URL + "/all_saved_posts", requestOptions)
+      .then(res => res.json())
+      .then(data => {
+        setLoading(false);
+        console.log("get  post request back is: ", data);
+        console.log(data);
+        if (data !== "failed") {
+          setPosts(data.sort((a,b)=>b.post_id-a.post_id));
+          setError(null);
+        } else {
+          //alert(data);
+          setError(data);
+        }
+      })
+      .catch(err => {
+        setLoading(false)
+        console.log("can not get  posts: " + err);
+        setError("Can not connect to server!");
+      });
+  }
+
+  function displayGenrePosts(genre) {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        auth_token: localStorage.getItem("auth_token"),
+        username: localStorage.getItem("username"),
+        genre: genre
+      }
+    };
+    fetch(API_URL + "/songs_by_genre", requestOptions)
+      .then(res => res.json())
+      .then(data => {
+        setLoading(false);
+        console.log("get  post request back is: ", data);
+        console.log(data);
+        if (data !== "failed") {
+          setPosts(data.sort((a,b)=>b.post_id-a.post_id));
+          setError(null);
+        } else {
+          //alert(data);
+          setError(data);
+        }
+      })
+      .catch(err => {
+        setLoading(false)
+        console.log("can not get  posts: " + err);
+        setError("Can not connect to server!");
+      });
+  }
+
+  function handleSelectChange(e) {
+    const selection = e.target.value;
+
+    if(selection === "feed") {
+      displayMyFeed();
+    } else if(selection === "saved") {
+      displaySavedPosts();
+    } else {
+      displayGenrePosts(selection);
+    }
+  }
+
 
 
   return (
@@ -82,6 +186,21 @@ export default function Feed() {
       <h5 className="feed-subtitle">
         Feed
       </h5>
+      <select className="feed-select" onChange={handleSelectChange}>
+        <option selected value="feed">My Feed</option>
+        <option value="saved">Saved Posts</option>
+        <option value="house">Songs by Genre: House</option>
+        <option value="techno">Songs by Genre: Techno</option>
+        <option value="pop">Songs by Genre: Pop</option>
+        <option value="rock">Songs by Genre: Rock</option>
+        <option value="alternative rock">Songs by Genre: Alternative Rock</option>
+        <option value="rnb">Songs by Genre: RnB</option>
+        <option value="trap">Songs by Genre: Trap</option>
+        <option value="hiphop">Songs by Genre: Hip Hop</option>
+        <option value="deep house">Songs by Genre: Deep House</option>
+        <option value="melodic techno">Songs by Genre: Melodic Techno</option>
+        <option value="progressive house">Songs by Genre: Progressive House</option>
+      </select>
             {posts.length === 0 ? <h4>No posts yet...</h4> : null}
 
             {posts.map(post => (
