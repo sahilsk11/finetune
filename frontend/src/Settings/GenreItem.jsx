87fe32bf826300aad6d1d66c528aed1dfdd4f1a1
qhyltong <qhyltong@purdue.edu>
2022-04-28 17:10:10 -0400

    Following genres fix

--- frontend/src/Settings/GenreItem.jsx
+++ frontend/src/Settings/GenreItem.jsx
@@ -4,19 +4,16 @@ import './settings.css';
 function GenreItem(props) {
 
   const [isFollowing, setIsFollowing] = useState(false);
-  //const genreTitle = props.genre.charAt(0).toUpperCase() + props.genre.slice(1);
+  const genreTitle = props.genre.charAt(0).toUpperCase() + props.genre.slice(1);
 
   useEffect(() => {
     if(props.followedGenres.includes(props.genre)) {
       setIsFollowing(true)
     }
-  })
+  }, [props.followedGenres])
 
   function handleClick() {
     let endpoint = isFollowing ? "/unfollow_genre" : "/follow_genre";
-    setIsFollowing(!isFollowing)
-
-
     fetch("http://localhost:5000"+endpoint, {
       method: "POST",
       headers: {
@@ -28,16 +25,20 @@ function GenreItem(props) {
       return response.json()
     }).then(data => {
       console.log(data)
+
     })
+    setIsFollowing(!isFollowing)
+
   }
 
   let text = isFollowing ? "Following" : "Follow";
 
 
+
   return(
     <div key={props.genre} className='genre-result'>
-      {props.genre}
-      <button onClick={handleClick} className='follow-genre-button'>{text}</button>
+      {genreTitle}
+      <button key={props.genre} onClick={handleClick} className='follow-genre-button'>{text}</button>
     </div>
   );
 }
